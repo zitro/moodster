9acb44f6e141b01fe3acc866e5cd98614ce3f16b
alin684 <al684@cornell.edu>
2018-03-07 15:03:28 -0500

    finally a picture

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,6 +1,12 @@
 class ImagesController < ApplicationController
   def create
-    @data = request.raw_post
+    # current_user.set_picture(request.raw_post)
+    # current_user.save
+    # byebug
+    # @data = request.raw_post
+    # @data1 = request.body
+    File.open("plzwork.jpg", 'wb') {|f| f.write request.body.read }
+
     # TODO: save this blob as jpeg file somewhere on the disk
     # render html: "received file that is #{data.length} bytes long"
   end
